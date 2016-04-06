e1c6324712291eba9aa161cd3ed130538ce81f8f
aqingsao <vinci.zhang@gmail.com>
2016-04-06 18:02:58 +0800

    show statistic details only when parameter is specified

--- nana.sh
+++ nana.sh
@@ -1,19 +1,53 @@
 #!/bin/sh
 
-if [ "$1" = "" ]; then
+showUrls=0
+showRequestCount=0
+showResponseSize=0
+showResponseTime=0
+showIp=0
+showSlowQueries=0
+while getopts ":pq:" optname
+  do
+    case "$optname" in
+      "c")
+        showRequestCount=1
+        ;;
+      "u")
+        showUrls=1
+        ;;
+      "s")
+        showResponseSize=1
+        ;;
+      "t")
+        showResponseTime=1
+        ;;
+      "s")
+        showResponseSize=1
+        ;;
+      "i")
+        showIp=1
+        ;;
+      "s")
+        showResponseSize=1
+        ;;
+      "q")
+        showSlowQueries=1
+        ;;
+      *)
+      # Should not occur
+        echo "Unknown error while processing options"
+        ;;
+    esac
+  done
+
+file="${@: -1}"
+if [ "$file" = "" ]; then
   echo "please provide an Nginx log file"
   exit 1;
 fi
-
-file="$1"
 if [ ! -f $file ]; then
   echo "File $file does not exist";
   exit 1;
-fi
-if [ "$2" = "" ]; then
-  seconds=2
-else
-  seconds=$2
 fi
 
 # echo "---------nginx summary---------"
@@ -31,47 +65,61 @@ echo "Response time: average ${resTimeAverage}s, max ${maxTimeOfRes}s"
 echo "Traffic: total ${bytesTotalInM}MB, max ${maxBytesInK}KB, average ${bytesAverageInK}KB/req"
 echo "Transfer rate: average ${rateInK}KB/s, max ${maxRateInK}KB/s, min${minRateInK}KB/s"
 
-# -m
-echo "\n---------Busy moments(By Seconds)---------"
-echo "Request Count \t Total Bytes \t Time Spent/req \t Moment \t"
-less $file | awk '{requests[$4]++; bytes[$4]+=$10; times[$4]+=$11;} END{for(i in requests){printf("%s %s %s %s\n", requests[i], bytes[i] / 1024, times[i]/requests[i], i)}}' | sort -nr | head -n 10
-echo "---------Busy moments(By Minutes)---------"
-echo "Request Count \t Total Bytes \t Time Spent/req \t Moment \t"
-less $file | awk '{minute = substr($4, 1, 18); requests[minute]++; bytes[minute]+=$10; times[minute]+=$11;} END{for(m in requests){printf("%s %sMB %ss %s\n", requests[m], bytes[m] / 1024 / 1024, times[m]/requests[m], m)}}' | sort -nr | head -n 10
+# -c
+if [ "${showRequestCount}" = "1" ]; then
+    echo "\n---------Request Count(By Seconds)---------"
+    echo "Request Count \t Total Bytes \t Time Spent/req \t Moment \t"
+    less $file | awk '{requests[$4]++; bytes[$4]+=$10; times[$4]+=$11;} END{for(s in requests){printf("%s %s %s %s\n", requests[s], bytes[s] / 1024, times[s]/requests[s], s)}}' | sort -nr | head -n 10
+    echo "---------Request Count(By Minutes)---------"
+    echo "Request Count \t Total Bytes \t Time Spent/req \t Moment \t"
+    less $file | awk '{minute = substr($4, 1, 18); requests[minute]++; bytes[minute]+=$10; times[minute]+=$11;} END{for(m in requests){printf("%s %sMB %ss %s\n", requests[m], bytes[m] / 1024 / 1024, times[m]/requests[m], m)}}' | sort -nr | head -n 10
+fi
 
-# -l
-echo "\n---------Top visited urls---------"
-echo "Request count \t Page Size/req \t url \t"
-less $file | awk '{printf("%s?%s\n", $10,$7)}' | awk -F '?' '{requests[$2]++;bytes[$2]+=$1} END{for(i in requests){printf("%s %sKB %s\n", requests[i], bytes[i] / requests[i] / 1024, i)}}' | sort -nr | head -n 10
+# -u
+if [ "${showUrls}" = "1" ]; then
+    echo "\n---------Top visited urls---------"
+    echo "Request count \t Page Size/req \t url \t"
+    less $file | awk '{printf("%s?%s\n", $10,$7)}' | awk -F '?' '{requests[$2]++;bytes[$2]+=$1} END{for(i in requests){printf("%s %sKB %s\n", requests[i], bytes[i] / requests[i] / 1024, i)}}' | sort -nr | head -n 10
+fi
 
 # -s
-echo "\n---------Response Size(By Total)---------"
-echo "Total Size \t Response Size/req \t requests count \t url"
-less $file | awk '{printf("%s?%s\n", $10,$7)}' | awk -F '?' '{requests[$2]++;bytes[$2]+=$1} END{for(i in requests){printf("%s %s %s %s\n", bytes[i] / 1024 / 1024, bytes[i] /requests[i] / 1024, requests[i], i)}}' | sort -nr | head -n 10 | awk '{printf("%sMB %sKB %s %s\n", $1, $2, $3, $4)}'
-echo "---------Response Size(By Average)---------"
-echo "Total Size \t Response Size/req \t requests count \t url"
-less $file | awk '{printf("%s?%s\n", $10,$7)}' | awk -F '?' '{requests[$2]++;bytes[$2]+=$1} END{for(i in requests){printf("%s %s %s %s\n", bytes[i] /requests[i] / 1024, bytes[i] / 1024 / 1024, requests[i], i)}}' | sort -nr | head -n 10 | awk '{printf("%sMB %sKB %s %s\n", $2, $1, $3, $4)}'
+if [ "${showResponseSize}" = "1" ]; then
+    echo "\n---------Response Size(By Total)---------"
+    echo "Total Size \t Response Size/req \t requests count \t url"
+    less $file | awk '{printf("%s?%s\n", $10,$7)}' | awk -F '?' '{requests[$2]++;bytes[$2]+=$1} END{for(i in requests){printf("%s %s %s %s\n", bytes[i] / 1024 / 1024, bytes[i] /requests[i] / 1024, requests[i], i)}}' | sort -nr | head -n 10 | awk '{printf("%sMB %sKB %s %s\n", $1, $2, $3, $4)}'
+    echo "---------Response Size(By Average)---------"
+    echo "Total Size \t Response Size/req \t requests count \t url"
+    less $file | awk '{printf("%s?%s\n", $10,$7)}' | awk -F '?' '{requests[$2]++;bytes[$2]+=$1} END{for(i in requests){printf("%s %s %s %s\n", bytes[i] /requests[i] / 1024, bytes[i] / 1024 / 1024, requests[i], i)}}' | sort -nr | head -n 10 | awk '{printf("%sMB %sKB %s %s\n", $2, $1, $3, $4)}'
+fi
 
 # -t
-echo "\n---------Reponse Time(By Total)---------"
-echo "Total Time \t Response Time/req \t requests count \t url"
-less $file | awk '{printf("%s?%s\n", $11,$7)}' | awk -F '?' '{requests[$2]++;times[$2]+=$1} END{for(i in requests){printf("%s %s %s %s\n", times[i], times[i] /requests[i], requests[i], i)}}' | sort -nr | head -n 10 | awk '{printf("%ss %ss %s %s\n", $1, $2, $3, $4)}'
-echo "---------Reponse Time(By Average)---------"
-echo "Total Time \t Response Time/req \t requests count \t url"
-less $file | awk '{printf("%s?%s\n", $11,$7)}' | awk -F '?' '{requests[$2]++;times[$2]+=$1} END{for(i in requests){printf("%s %s %s %s\n", times[i] /requests[i], times[i], requests[i], i)}}' | sort -nr | head -n 10 | awk '{printf("%ss %ss %s %s\n", $2, $1, $3, $4)}'
+if [ "${showResponseTime}" = "1" ]; then
+    echo "\n---------Reponse Time(By Total)---------"
+    echo "Total Time \t Response Time/req \t requests count \t url"
+    less $file | awk '{printf("%s?%s\n", $11,$7)}' | awk -F '?' '{requests[$2]++;times[$2]+=$1} END{for(i in requests){printf("%s %s %s %s\n", times[i], times[i] /requests[i], requests[i], i)}}' | sort -nr | head -n 10 | awk '{printf("%ss %ss %s %s\n", $1, $2, $3, $4)}'
+    echo "---------Reponse Time(By Average)---------"
+    echo "Total Time \t Response Time/req \t requests count \t url"
+    less $file | awk '{printf("%s?%s\n", $11,$7)}' | awk -F '?' '{requests[$2]++;times[$2]+=$1} END{for(i in requests){printf("%s %s %s %s\n", times[i] /requests[i], times[i], requests[i], i)}}' | sort -nr | head -n 10 | awk '{printf("%ss %ss %s %s\n", $2, $1, $3, $4)}'
+fi
 
-# -f
-echo "\n---------Traffic(By Total)---------"
-echo "Total Size \t Response Size/req \t requests count \t url"
-less $file | awk '{printf("%s?%s\n", $10,$7)}' | awk -F '?' '{requests[$2]++;bytes[$2]+=$1} END{for(i in requests){printf("%s %s %s %s\n", bytes[i] / 1024 / 1024, bytes[i] /requests[i] / 1024, requests[i], i)}}' | sort -nr | head -n 10 | awk '{printf("%sMB %sKB %s %s\n", $1, $2, $3, $4)}'
+# # -f
+# if [ "${showUrls}" = "1" ]; then
+#     echo "\n---------Traffic(By Total)---------"
+#     echo "Total Size \t Response Size/req \t requests count \t url"
+#     less $file | awk '{printf("%s?%s\n", $10,$7)}' | awk -F '?' '{requests[$2]++;bytes[$2]+=$1} END{for(i in requests){printf("%s %s %s %s\n", bytes[i] / 1024 / 1024, bytes[i] /requests[i] / 1024, requests[i], i)}}' | sort -nr | head -n 10 | awk '{printf("%sMB %sKB %s %s\n", $1, $2, $3, $4)}'
+# fi
 
 # -i
-echo "\n---------Ip Addresses---------"
-ipCount=`less $file | awk '{print $1}' | sort | uniq | wc -l`
-echo "${ipCount} unique ip addresses"
-echo "IP \t requests count"
-less $file | awk '{requests[$1]++} END{for(ip in requests){printf("%s %s\n", ip, requests[ip])}}' | sort -nr | head -n 10
+if [ "${showIp}" = "1" ]; then
+    echo "\n---------Ip Addresses---------"
+    ipCount=`less $file | awk '{print $1}' | sort | uniq | wc -l`
+    echo "${ipCount} unique ip addresses"
+    echo "requests count \t Ip address"
+    less $file | awk '{requests[$1]++} END{for(ip in requests){printf("%s %s\n", requests[ip], ip)}}' | sort -nr | head -n 10
+fi
 
 # -q
-echo "\n---------slow queries---------"
-less $file | awk -v limit=$seconds '{lines[NR]=$0;time[NR]=$11;slowCount=0;if($11 >= limit && NR >= 10){for(i = 10; i >=0; i--){if(time[NR-i] >= limit){slowCount++}; }}; if(slowCount >=6 ){for(i = 10; i >=0; i--){print lines[NR-i]}}}' | more
+if [ "${showSlowQueries}" = "1" ]; then
+    echo "\n---------slow queries(>=3 seconds for 8 out of 10 times)---------"
+    less $file | awk -v limit=3 '{lines[NR]=$0;time[NR]=$11;slowCount=0;if($11 >= limit && NR >= 10){for(i = 10; i >=0; i--){if(time[NR-i] >= limit){slowCount++}; }}; if(slowCount >=6 ){for(i = 10; i >=0; i--){print lines[NR-i]}}}' | more
+fi
