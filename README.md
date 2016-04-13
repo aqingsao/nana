aaa20937072d40328ffb18f0b5076c342e883875
aqingsao <vinci.zhang@gmail.com>
2016-04-13 16:00:22 +0800

    refactor and update README.md

--- README.md
+++ README.md
@@ -2,56 +2,90 @@
 Nana is an Nginx log analyzer to identify performance bottlenecks. It's written in shell and awk, aiming to be the best Nginx log analyzer.
 
 So far the following metrics are collected:
-#### Nginx summary
-- count of requests and unique ip addresses;
-- count of slow requests;
-- Total bytes, average bytes per request and transfer rate
 
-#### Busy moments
-- List top 10 moments that are visited most frequently
+#### Page visits
+- Top 10 moments that have highest page visits
+- Top 10 pages that are visited most frequently
 
-#### Hot urls
-- List top 10 urls that are mostly visited
+#### Traffic and rate
+- Top 10 moments that have highest traffic
+- Top 10 moments that have fastest rate
+- Top 10 urls that have largest total resonse size
+- Top 10 urls that have largest average response size
 
-#### Large urls
-- List top 10 urls that have largest response size
-
-#### Slow urls
-- List top 10 urls that have largest response time
+#### Response time
+- List top 10 urls that takes longest response time in total
+- List top 10 urls that takes longest response time in average
 
 #### Slow requests
 - List slow requests, avoiding mistakes caused by poor network conditions of mobile phones.
 
- 
-# Usages
-`./nana.sh <logfile> [timeLimitInSeconds]`
 
-Where <logfile> is the absolute location of nginx log file; [timeLImitInSeconds] is optional with a default value of 3 seconds
+# Usages
+`./nana.sh <options> logfile`
+
+Where <logfile> is the absolute location of nginx log file;
 
 For example: 
 `./nana.sh /var/log/nginx/main.log`
 
 And here is an exmample output: 
 
-`---------nginx summary---------  
-requests: total 78970, slow 178, peak 350/s  
-unique ip: total 4229, slow 53  
-time: average 0.205734s, max 73.528s  
-traffic: total 1158.1MB, max 1417.18KB, average 72.9927KB/req  
-rate: average 4186.4KB/s, max 757.32KB/s, min 0KB/s  
-
----------Peak moments---------  
-...  
----------Hot urls---------  
-...  
----------slow summary---------  
-58.244.**.** - - [22/Mar/2016:14:43:17 +0800] "GET /url HTTP/1.1" 200 28 5.801 5.801 "http://host/url1" "Mozilla/5.0 (iPhone; CPU iPhone OS 9_2_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Mobile/13D15" "-"  
-...  
+`---------nginx summary---------
+page visits(-p for detail):
+      total 888510, average 43.7022/s
+      peak 1421/s at [13/Apr/2016:10:47:23
+      max count 203192 of url ***
+traffic and rate(-r for detail):
+      total traffic 1903.55MB, average traffic 2.19383KB/req
+      highest traffic 292.069MB of url ***
+      average rate 147.204KB/s
+      max rate 9254.88KB/s at [13/Apr/2016:15:17:45
+response time(-t for detail):
+      total 132412.7s, average 0.149033s/req
+      max total time 12224.93s of url ***
+      slowest response time 12.266s/req of url ***
+      slowest response time 4.50s/req at [13/Apr/2016:09:35:20
+ip addresses(-i for detail):
+      unique ip addresses count 33413
+      max requests 4689 from ip ***
 `
 
+#### -p: show page visits details  
+`nana.sh -p /var/log/nginx/main.log`  
+Example output:  
+`
+---------By Seconds:  
+Page Visits \t Response Size \t Time Spent/req \t Moment \t  
+1420 1519.83KB 0.00802817 [13/Apr/2016:10:47:23  
+490 2880.27KB 0.0187551 [13/Apr/2016:13:27:12  
+...  
+---------By Minutes:  
+---------Page Visits \t Response Size \t Time Spent/req \t Moment \t  
+9731 1.06466MB 0.149084s [13/Apr/2016:10:17  
+...  
+
+---------By Url:  
+---------Page Visits \t Page Size/req \t url \t  
+234442 0.335132KB /api/***  
+...
+`
+
+#### -r: show traffic and rate details
+`nana.sh -r /var/log/nginx/main.log`
+
+#### -t: show response time details  
+`nana.sh -t /var/log/nginx/main.log`
+
+#### -s: show slow queries
+`nana.sh -s /var/log/nginx/main.log`
+
 # TODO List
-- Show urls that has longest average response time
-- List 500/404 pages
+- List 5**/4**/3** pages
+- Show moments that traffic rate is limited by bandwith
+- Show moments that upstream servers are slow
+- Show access frequency of crawlers(google bot, baidu spider, bingbot, MJ12bot, YandexBot)
+- Show market shares of various mobile phones(iphone/android/win phone...); and portions of versions(iPhone 5, 6, 7, 8, 9)/manufactures(Huawei, Samsung, iPhone, Nexus, Letv, HTC, vivo, Xiaomi)
 
 # FAQ
 
--- nana.sh
+++ nana.sh
@@ -4,44 +4,44 @@ function printHelp()
 {
     echo "Usage:"
     echo "-h: usage"
-    echo "-a: show all details(combination of c/r/u/s/t/i/r/q...)"
-    echo "-c: request count(by seconds and by minutes, and by urls)"
-    echo "-r: traffic rate(By seconds, by minutes, and by urls)"
-    echo "-u: urls(top visited urls)"
+    echo "-a: show all details"
+    echo "-p: page visits"
+    echo "-r: traffic rate"
     echo "-t: response time"
     echo "-i: ip addresses"
-    echo "-r: traffic rate(by seconds and minutes)"
-    echo "-q: slow queries(over 3 seconds for 8 out of 10 continuous requests)"
+    echo "-s: slow queries(over 3 seconds for 8 out of 10 continuous requests)"
     echo ""
 }
 
 showUrls=0
-showRequestCount=0
+showPageVisits=0
 showTrafficRate=0
+showResponseTime=0
 showIp=0
 showSlowQueries=0
-while getopts ":crustiqh" optname
+while getopts ":haprist" optname
   do
+    echo "$optname"
     case "$optname" in
-      "c")
-        showRequestCount=1
+      "p")
+        showPageVisits=1
         ;;
       "r")
         showTrafficRate=1
         ;;
-      "u")
-        showUrls=1
+      "t")
+        showResponseTime=1
         ;;
       "i")
         showIp=1
         ;;
-      "q")
+      "s")
         showSlowQueries=1
         ;;
       "a")
-        showUrls=1
-        showRequestCount=
+        showPageVisits=1
         showTrafficRate=1
+        showResponseTime=1
         showIp=1
         showSlowQueries=1
         ;;
@@ -76,123 +76,123 @@ echo "---------nginx summary---------"
 ipCount=`less $file | awk '{print $1}' | sort | uniq | wc -l`
 slowIpCount=`less $file | awk -v limit="$seconds" '{if($11>limit){print $1}}' | sort | uniq | wc -l`
 
-
-read countTotal, avrCountBySec,maxRCountSec, maxRCountBySec, maxRCountUrl, maxRCountByUrl,  bytesTotal, avrRBytesBySec, maxBytesSec, maxBytesBySec, maxRBytesUrl, maxRBytesByUrl, timeTotal <<< `less $file | awk -v limit=3 'BEGIN{maxTime=0; maxRate=0;maxBytes=0} 
-{sec =$4;rTime=$11; rBytes=$10; split($7,urls,"?"); url=urls[1];
-    countTotal++;timeTotal+=rTime; bytesTotal+=rBytes; 
-    rCountBySec[sec]++; rCountByUrl[url]++; 
-    rBytesBySec[sec]+=rBytes; rBytesByUrl[url]+=rBytes;
+# m: max; a: average; c: count
+# S: second; U: url; R: request; T: total
+# 
+read countTotal avgCountByS maxCountS maxCountByS maxCountU maxCountByU bytesTotal avgBytesByR avgRateByS maxRateS maxRateByS maxBytesU maxBytesByU timeTotal avgTimeByR maxAvgTimeS maxAvgTimeByS maxTimeU maxTimeByU maxAvgTimeU maxAvrTimeByU uniqIpCount maxCountIp maxCountByIp<<< `less $file | awk -v limit=3 'BEGIN{maxTime=0; maxRate=0;maxBytes=0} 
+{ip=$1;sec =$4;time=$11; bytes=$10; split($7,urls,"?"); url=urls[1];
+    countTotal++;timeTotal+=time; bytesTotal+=bytes; 
+    countByS[sec]++; countByU[url]++; countByIp[ip]++;
+    bytesByS[sec]+=bytes; bytesByU[url]+=bytes;
+    timeByS[sec]+=time;timeByU[url]+=time;
 } 
-END{maxRCountSec="";maxRCountBySec=0; 
-    for(s in rCountBySec){if(rCountBySec[s] > maxRCountBySec){maxRCountBySec = rCountBySec[s]; maxRCountSec=s}}; 
-    avrCountBySec=countTotal/length(rCountBySec);maxRCountUrl="";maxRCountByUrl=0;
-    for(u in rCountByUrl){if(rCountByUrl[u] > maxRCountByUrl){maxRCountByUrl = rCountByUrl[u]; maxRCountUrl=u}}; 
-    avrRBytesBySec=bytesTotal/timeTotal; maxBytesSec="";maxBytesBySec=0;
-    for(s in rBytesBySec){if(rBytesBySec[s] > maxBytesBySec){maxBytesBySec = rBytesBySec[s]; maxBytesSec=s}}; 
-    maxRBytesUrl="";maxRBytesByUrl=0;
-    for(u in rBytesByUrl){if(rBytesByUrl[u] > maxRCountByUrl){maxRBytesUrl = rBytesByUrl[u]; maxRBytesUrl=u}}; 
-    
-    print countTotal, avrCountBySec, maxRCountSec, maxRCountBySec, maxRCountUrl, maxRCountByUrl, 
-        bytesTotal/1024/1024, avrRBytesBySec/1024, maxBytesSec, maxBytesBySec/1024, maxRBytesUrl, maxRBytesByUrl/1024/1024, 
-        timeTotal
+END{maxCountS="";maxCountByS=0; 
+    for(s in countByS){if(countByS[s] > maxCountByS){maxCountByS = countByS[s]; maxCountS=s}}; 
+    avgCountByS=countTotal/length(countByS);maxCountU="";maxCountByU=0;
+    for(u in countByU){if(countByU[u] > maxCountByU){maxCountByU = countByU[u]; maxCountU=u}}; 
+    avgBytesByR=bytesTotal/countTotal; 
+    maxBytesU="";maxBytesByU=0;
+    for(u in bytesByU){if(bytesByU[u] > maxBytesByU){maxBytesByU = bytesByU[u]; maxBytesU=u}}; 
+    avgRateByS=bytesTotal/timeTotal;maxRateS="";maxRateByS=0;
+    for(s in bytesByS){if(timeByS[s] > 0 && bytesByS[s]/timeByS[s] > maxRateByS){maxRateByS = bytesByS[s]/timeByS[s]; maxRateS=s}}; 
+    avgTimeByR=timeTotal/countTotal;maxAvgTimeS="";maxAvgTimeByS=0;maxTimeU="";maxTimeByU=0;maxAvgTimeU="";maxAvrTimeByU=0;
+    for(u in timeByU){if(timeByU[u] > maxTimeByU){maxTimeByU = timeByU[u]; maxTimeU=u}; if(timeByU[u]/countByU[u] > maxAvrTimeByU){maxAvrTimeByU=timeByU[u]/countByU[u]; maxAvgTimeU=u}}; 
+    for(s in timeByS){if(timeByS[s]/countByS[s] > maxAvgTimeByS){maxAvgTimeByS = timeByS[s]/countByS[s]; maxAvgTimeS=s}}; 
+    uniqIpCount=length(countByIp);maxCountIp="";maxCountByIp=0;
+    for(ip in countByIp){if(countByIp[ip] > maxCountByIp){maxCountByIp = countByIp[ip]; maxCountIp=ip}}; 
+
+    print countTotal, avgCountByS, maxCountS, maxCountByS, maxCountU, maxCountByU, 
+        bytesTotal/1024/1024, avgBytesByR/1024, avgRateByS/1024, maxRateS, maxRateByS/1024, maxBytesU, maxBytesByU/1024/1024, 
+        timeTotal, avgTimeByR, maxAvgTimeS, maxAvgTimeByS, maxTimeU, maxTimeByU, maxAvgTimeU, maxAvrTimeByU,
+        uniqIpCount, maxCountIp, maxCountByIp
         }'
 `
 
-# read reqsCount averageReqsPerSec peakReqsPerSec topVisitedUrl countOfTopVisitedUrl rateInK maxRateInK slowReqsCount resTimeAverage maxTimeOfRes bytesTotalInM maxBytesInK maxBytesLine bytesAverageInK <<< `less $file | awk -v limit=3 
-# 'BEGIN{maxTime=0; maxRate=0;maxBytes=0} 
-# {second =$4;responseTime=$11; responseBytes=$10; split($0,urls,"?"); url=urls[1];
-#     totalResponseTime+=responseTime; totalResponseBytes+=responseBytes; 
-#     reqsCountPerSec[second]++; responseBytesPerSec[second]=responseBytes;
-#     reqsCountPerUrl[url]++; responseBytesPerUrl[url]=responseBytes;
-#     if(responseTime>0){rate = responseBytes/responseTime}; 
-#     if(responseTime>maxTime){maxTime=responseTime;}; 
-#     if(responseTime>limit){slowReqs++}; 
-#     if(responseTime > 0 && rate>maxRate){maxRate=rate;}; 
-#     if(responseBytes>maxBytes){maxBytes=responseBytes;}; 
-# } 
-# END{averagereqsCountPerSec=FNR/length(reqsCountPerSec);
-#     peakreqsCountPerSec=0; 
-#     for(second in reqsCountPerSec){if(reqsCountPerSec[second] > peakreqsCountPerSec){peakreqsCountPerSec = reqsCountPerSec[second]}}; 
-#     peakResponseBytesPerSec=0;
-#     for(second in responseBytesPerSec){if(responseBytesPerSec[second] > peakResponseBytesPerSec){peakResponseBytesPerSec = responseBytesPerSec[second]}}; 
-#     topVisitedUrl="";countOfTopVisitedUrl=0;
-#     for(url in reqsCountPerUrl){if(reqsCountPerUrl[url] > countOfTopVisitedUrl){countOfTopVisitedUrl = reqsCountPerUrl[url]; topVisitedUrl=url}}; 
-#     print FNR, averagereqsCountPerSec, peakreqsCountPerSec, topVisitedUrl, countOfTopVisitedUrl, 
-#         totalResponseBytes /totalResponseTime/1024, maxRate/1024, slowReqs, totalResponseTime/ FNR, maxTime, totalResponseBytes / 1024 / 1024, maxBytes/1024, bytes / FNR / 1024}'
-# `
+echo "page visits(-p for detail): "
+echo "      total ${countTotal}, average ${avgCountByS}/s"
+echo "      peak ${maxCountByS}/s at ${maxCountS}"
+echo "      max count ${maxCountByU} of url ${maxCountU}"
+echo "traffic and rate(-r for detail): "
+echo "      total traffic ${bytesTotal}MB, average traffic ${avgBytesByR}KB/req"
+echo "      highest traffic ${maxBytesByU}MB of url ${maxBytesU}"
+echo "      average rate ${avgRateByS}KB/s"
+echo "      max rate ${maxRateByS}KB/s at ${maxRateS}"
+echo "response time(-t for detail): "
+echo "      total ${timeTotal}s, average ${avgTimeByR}s/req"
+echo "      max total time ${maxTimeByU}s of url ${maxTimeU}"
+echo "      slowest response time ${maxAvrTimeByU}s/req of url ${maxAvrTimeU}"
+echo "      slowest response time ${maxAvgTimeByS}s/req at ${maxAvgTimeS}"
+echo "ip addresses(-i for detail): "
+echo "      unique ip addresses count ${uniqIpCount}"
+echo "      max requests ${maxCountByIp} from ip ${maxCountIp}"
+echo "slow queries(-s for detail): "
+# echo "ip addresses: uniq count $ipCount, slow $slowIpCount"
+# echo "slow requests(-q), slow $slowReqsCount, rate "
 
-echo "(-c)request count: total $countTotal, average ${avrCountBySec}/s, peak ${maxRCountBySec}/s at ${maxRCountSec}, max count ${maxRCountByUrl} of url ${maxRCountUrl}"
-echo "(-r)traffic rate: total ${bytesTotal}MB, average ${avrRBytesBySec}KB/s, max ${maxBytesBySec}KB/s at ${maxBytesSec}, highest traffic ${maxRBytesByUrl}MB of url ${maxRBytesUrl}"
-echo "ip addresses: uniq count $ipCount, slow $slowIpCount"
-echo "time: average ${resTimeAverage}s, max ${maxTimeOfRes}s"
-echo "traffic: total ${bytesTotalInM}MB, max ${maxBytesInK}KB, average ${bytesAverageInK}KB/req"
-echo "slow requests(-q), slow $slowReqsCount, rate "
-echo ""
-
-# -c
-if [ "${showRequestCount}" = "1" ]; then
-    echo "---------Request Count(By Seconds)---------"
-    echo "Request Count \t Response Size \t Time Spent/req \t Moment \t"
-    less $file | awk '{second=$4;requests[second]++; bytes[second]+=$10; times[second]+=$11;} END{for(s in requests){printf("%s %sKB %s %s\n", requests[s], bytes[s] / 1024, times[s]/requests[s], s)}}' | sort -nr | head -n 10
-    echo "---------Request Count(By Minutes)---------"
-    echo "Request Count \t Total Bytes \t Time Spent/req \t Moment \t"
-    less $file | awk '{minute = substr($4, 1, 18); requests[minute]++; bytes[minute]+=$10; times[minute]+=$11;} END{for(m in requests){printf("%s %sMB %ss %s\n", requests[m], bytes[m] / 1024 / 1024, times[m]/requests[m], m)}}' | sort -nr | head -n 10
+# -p
+if [ "${showPageVisits}" = "1" ]; then
     echo ""
-    echo "---------Request Count(By url)---------"
-    echo "Request count \t Page Size/req \t url \t"
+    echo "---------Page Visits Details---------"
+    echo "---------By Seconds:"
+    echo "Page Visits \t Response Size \t Time Spent/req \t Moment \t"
+    less $file | awk '{second=$4;requests[second]++; bytes[second]+=$10; times[second]+=$11;} END{for(s in requests){printf("%s %sKB %s %s\n", requests[s], bytes[s] / 1024, times[s]/requests[s], s)}}' | sort -nr | head -n 10
+    
+    echo ""
+    echo "---------By Minutes:"
+    echo "---------Page Visits \t Response Size \t Time Spent/req \t Moment \t"
+    less $file | awk '{minute = substr($4, 1, 18); requests[minute]++; bytes[minute]+=$10; times[minute]+=$11;} END{for(m in requests){printf("%s %sMB %ss %s\n", requests[m], bytes[m] / 1024 / 1024, times[m]/requests[m], m)}}' | sort -nr | head -n 10
+    
+    echo ""
+    echo "---------By Url:"
+    echo "---------Page Visits \t Page Size/req \t url \t"
     less $file | awk '{printf("%s?%s\n", $10,$7)}' | awk -F '?' '{requests[$2]++;bytes[$2]+=$1} END{for(i in requests){printf("%s %sKB %s\n", requests[i], bytes[i] / requests[i] / 1024, i)}}' | sort -nr | head -n 10
+    echo "---------End of Page Visits Details---------"
     echo ""
 fi
 
 # -r
 if [ "${showTrafficRate}" = "1" ]; then
-    echo "---------Response size(By Seconds)---------"
-    echo "Traffic Total \t Traffic Rate \t Moment \t"
-    less $file | awk '{second=$4;bytes[second]+=$10;} END{for(s in bytes){printf("%s %s\n", bytes[s], s)}}' | sort -nr | head -n 10 | awk '{printf("%sKB %sKB %s\n", $1 / 1024, $1 / 1024, $2)}'
-    echo "---------Response Size(By Minutes)---------"
-    echo "Traffic Total \t Traffic Rate \t Moment \t"
-    less $file | awk '{minute = substr($4, 1, 18); bytes[minute]+=$10;} END{for(m in bytes){printf("%s %s\n", bytes[m], m)}}' | sort -nr | head -n 10 | awk '{printf("%sMB %sKB %s\n", $1 / 1024 / 1024, $1 / 60 / 1024, $2)}'
-
-    echo "---------Response Size(By Total)---------"
-    echo "Total Size \t Response Size/req \t requests count \t url"
-    less $file | awk '{printf("%s?%s\n", $10,$7)}' | awk -F '?' '{url=$2; requests[url]++;bytes[url]+=$1} END{for(url in requests){printf("%s %s %s %s\n", bytes[url] / 1024 / 1024, bytes[url] /requests[url] / 1024, requests[url], url)}}' | sort -nr | head -n 10 | awk '{printf("%sMB %sKB %s %s\n", $1, $2, $3, $4)}'
-    echo "---------Response Size(By Average)---------"
-    echo "Total Size \t Response Size/req \t requests count \t url"
-    less $file | awk '{printf("%s?%s\n", $10,$7)}' | awk -F '?' '{url=$2; requests[url]++;bytes[url]+=$1} END{for(url in requests){printf("%s %s %s %s\n", bytes[url] /requests[url] / 1024, bytes[url] / 1024 / 1024, requests[url], url)}}' | sort -nr | head -n 10 | awk '{printf("%sMB %sKB %s %s\n", $2, $1, $3, $4)}'
     echo ""
+    echo "---------Traffic and Rate Details---------"
+    echo "---------By Traffic"
+    echo "Traffic \t Rate \t Moment \t"
+    less $file | awk '{second=$4;bytes[second]+=$10;time[second]+=$11} END{for(s in bytes){printf("%s %s %s\n", bytes[s], bytes[s]/time[s], s)}}' | sort -nr | head -n 10 | awk '{printf("%sKB %sKB/s %s\n", $1 / 1024, $2 / 1024, $3)}'
+
+    echo ""
+    echo "---------By Rate"
+    echo "Rate \t Traffic \t Moment \t"
+    less $file | awk '{second=$4;bytes[second]+=$10;time[second]+=$11} END{for(s in time){printf("%s %s %s\n", bytes[s]/time[s], bytes[s], s)}}' | sort -nr | head -n 10 | awk '{printf("%sKB/s %sKB %s\n", $1 / 1024, $2 / 1024, $3)}'
+
+    echo ""
+    echo "---------By Url(total)"
+    echo "Total Response Size \t Response Size/req \t requests count \t url"
+    less $file | awk '{printf("%s?%s\n", $10,$7)}' | awk -F '?' '{url=$2; requests[url]++;bytes[url]+=$1} END{for(url in requests){printf("%s %s %s %s\n", bytes[url] / 1024 / 1024, bytes[url] /requests[url] / 1024, requests[url], url)}}' | sort -nr | head -n 10 | awk '{printf("%sMB %sKB %s %s\n", $1, $2, $3, $4)}'
+    
+    echo ""
+    echo "---------By Url(average)"
+    echo "Response Size/req \t Total Response Size \t requests count \t url"
+    less $file | awk '{printf("%s?%s\n", $10,$7)}' | awk -F '?' '{url=$2; requests[url]++;bytes[url]+=$1} END{for(url in requests){printf("%s %s %s %s\n", bytes[url] /requests[url] / 1024, bytes[url] / 1024 / 1024, requests[url], url)}}' | sort -nr | head -n 10 | awk '{printf("%sKB %sMB %s %s\n", $1, $2, $3, $4)}'
+    echo "---------End of Traffic and Rate Details---------"
 fi
 
 # -t
 if [ "${showResponseTime}" = "1" ]; then
-    echo "---------Reponse Time(By Total)---------"
+    echo ""
+    echo "---------Reponse Time Details---------"
+    echo "---------By Url(total)"
     echo "Total Time \t Response Time/req \t requests count \t url"
     less $file | awk '{printf("%s?%s\n", $11,$7)}' | awk -F '?' '{requests[$2]++;times[$2]+=$1} END{for(i in requests){printf("%s %s %s %s\n", times[i], times[i] /requests[i], requests[i], i)}}' | sort -nr | head -n 10 | awk '{printf("%ss %ss %s %s\n", $1, $2, $3, $4)}'
-    echo "---------Reponse Time(By Average)---------"
-    echo "Total Time \t Response Time/req \t requests count \t url"
-    less $file | awk '{printf("%s?%s\n", $11,$7)}' | awk -F '?' '{requests[$2]++;times[$2]+=$1} END{for(i in requests){printf("%s %s %s %s\n", times[i] /requests[i], times[i], requests[i], i)}}' | sort -nr | head -n 10 | awk '{printf("%ss %ss %s %s\n", $2, $1, $3, $4)}'
-    echo ""
-fi
 
-# -u
-if [ "${showUrls}" = "1" ]; then
-    echo "---------Top visited urls---------"
-    echo "Request count \t Page Size/req \t url \t"
-    less $file | awk '{printf("%s?%s\n", $10,$7)}' | awk -F '?' '{requests[$2]++;bytes[$2]+=$1} END{for(i in requests){printf("%s %sKB %s\n", requests[i], bytes[i] / requests[i] / 1024, i)}}' | sort -nr | head -n 10
     echo ""
+    echo "---------By Url(average)"
+    echo "Response Time/req \t Total Time \t requests count \t url"
+    less $file | awk '{printf("%s?%s\n", $11,$7)}' | awk -F '?' '{requests[$2]++;times[$2]+=$1} END{for(i in requests){printf("%s %s %s %s\n", times[i] /requests[i], times[i], requests[i], i)}}' | sort -nr | head -n 10 | awk '{printf("%ss %ss %s %s\n", $1, $2, $3, $4)}'
+    echo "---------End of Reponse Time Details---------"
 fi
-
-# # -f
-# if [ "${showUrls}" = "1" ]; then
-#     echo "\n---------Traffic(By Total)---------"
-#     echo "Total Size \t Response Size/req \t requests count \t url"
-#     less $file | awk '{printf("%s?%s\n", $10,$7)}' | awk -F '?' '{requests[$2]++;bytes[$2]+=$1} END{for(i in requests){printf("%s %s %s %s\n", bytes[i] / 1024 / 1024, bytes[i] /requests[i] / 1024, requests[i], i)}}' | sort -nr | head -n 10 | awk '{printf("%sMB %sKB %s %s\n", $1, $2, $3, $4)}'
-# fi
 
 # -i
 if [ "${showIp}" = "1" ]; then
-    echo "---------Ip Addresses(visit times)---------"
-    ipCount=`less $file | awk '{print $1}' | sort | uniq | wc -l`
-    echo "${ipCount} unique ip addresses"
+    echo "---------Ip Addresses Details---------"
     echo "Requests count \t Ip address"
     less $file | awk '{requests[$1]++} END{for(ip in requests){printf("%s %s\n", requests[ip], ip)}}' | sort -nr | head -n 10
     echo ""
